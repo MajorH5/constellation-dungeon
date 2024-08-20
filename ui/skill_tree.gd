8551e22385d51999f35824f968f8b6fa01254085
Sabhee Rehman <34558423+SabheeR@users.noreply.github.com>
2024-08-19 22:35:58 -0400

    added stuff to skill tree script...

--- ui/skill_tree.gd
+++ ui/skill_tree.gd
@@ -1,5 +1,18 @@
 extends Control
 
+const TREE_DEFAULT_POS = Vector2.ZERO
+const TREE_CONTAINER_POS = Vector2(-262, 0)
+
+@onready var tree = $tree
+
+@onready var unlocks = {$btn_attack_1: true,
+		$btn_attack_1/btn_speed_1: false, "Speed2": false, "Speed3": false,
+		"Health1": false, "Health2": false, "Health3": false,
+		"Attack1": false, "Attack2": false, "Attack3": false,
+		"Radiant": false, "Restoration": false, "Reaper": false, "Icarus": false,
+		"Ascendent": false}
+
+var tree_dict
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +22,15 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+	
+func show_unlocked():
+	$btn_attack_1
+
+func open_tree():
+	tree.position = TREE_DEFAULT_POS
+	tree.visible = true
+	visible = false
+
+func close():
+	tree.visible = false
+	visible = false
